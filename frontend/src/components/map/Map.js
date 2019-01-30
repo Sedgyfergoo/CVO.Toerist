f30b69af2ea756576c828176a91b1172dfacc6d9
Tars <tars.hoeykens@crossworx.be>
2019-01-30 11:36:40 +0100

    map zonder tilelayer

--- frontend/src/components/map/Map.js
+++ frontend/src/components/map/Map.js
@@ -1,28 +1,26 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
-import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
 
 export default class Map extends Component {
+  render() {
+    const { Center } = this.props;
 
-    render() {
-        const { Center } = this.props;
-
-        return (
-            <Fragment>
-                <LeafletMap
-                    center={Center}
-                    zoom={1}
-                    maxZoom={10}
-                    attributionControl={true}
-                    zoomControl={true}
-                    doubleClickZoom={true}
-                    scrollWheelZoom={true}
-                    dragging={true}
-                    animate={true}
-                    easeLinearity={0.35}
-                />
-
-            </Fragment>
-        )
-    }
+    return (
+      <Fragment>
+        <LeafletMap
+          center={Center}
+          zoom={1}
+          maxZoom={10}
+          attributionControl={true}
+          zoomControl={true}
+          doubleClickZoom={true}
+          scrollWheelZoom={true}
+          dragging={true}
+          animate={true}
+          easeLinearity={0.35}
+        />
+      </Fragment>
+    );
+  }
 }
